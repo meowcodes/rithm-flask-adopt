2cab896a4549071d1c571506b2c752cc13ee7e8d
Gabriela <balicas.gb@gmail.com>
2019-02-22 11:59:29 -0800

    create pet model

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 FROM flask_sqlalchemy IMPORT SQLAlchemy
 db= SQLAlchemy()
 
+DEFAULT_IMG_URL = 'https://www.top13.net/wp-content/uploads/2015/10/perfectly-timed-cat-photos-funny-cover.jpg'
+
 def connect_db(app):
     """ Connects to database """
 
@@ -9,4 +11,19 @@ def connect_db(app):
 
 
 class Pet(db.Model):
+    
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    name = db.Column(db.String(30), 
+                    nullable=False)
+    species = db.Column(db.String(30),                              nullable=False)
+    photo_url = db.Column(db.String(200), 
+                    default=DEFAULT_IMG_URL)
+    age = db.Column(db.Integer, nullable=False)
+    notes = db.Column(db.Text)
+    available = db.Column(db.Boolean, nullable=False,
+                    default=True)
     
