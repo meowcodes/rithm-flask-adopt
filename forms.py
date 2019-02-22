4e26e90d9cc57d4bc0d0311376bb94284d281253
Gabriela <balicas.gb@gmail.com>
2019-02-22 15:05:02 -0800

    add additional validations

--- forms.py
+++ forms.py
@@ -1,16 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, TextAreaField
-from wtforms.validators import DataRequired, InputRequired, Optional, URL
+from wtforms import StringField, IntegerField, TextAreaField, SelectField
+from wtforms.validators import DataRequired, InputRequired, Optional, URL, Length
 
 
 class AddPetForm(FlaskForm):
     """ Form for adding new pets """
     pet_name = StringField("Pet name", 
                 validators=[InputRequired()])
-    species = StringField("Species", 
-                validators=[InputRequired()])
+    species = SelectField('Species',
+                choices=[('cat', 'Cat'), ('dog', 'Dog'), ('porcupine', 'Porcupine')], validators=[InputRequired()])
     photo_url = StringField("Photo URL", 
                 validators=[Optional(), URL(require_tld=True)])
-    age = IntegerField("Age", 
-                validators=[InputRequired()])
+    age = IntegerField("Age",
+                validators=[InputRequired(), Length(min=0, max=30)])
     notes = TextAreaField("Notes")
